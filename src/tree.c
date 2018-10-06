f6e46e659a6133ebc6b03e6baebba14e7475af4e
romain.baguet <romain.baguet@epita.fr>
2018-10-06 18:41:50 +0200

    tree.c respects coding style

--- src/tree.c
+++ src/tree.c
@@ -1,58 +1,84 @@
 #include "functions.h"
 
-struct record {
+struct record
+{
     int dir;
     int files;
 };
+
+struct treestruct
+{
+    int prof;
+    char *name;
+    int name_len;
+    char dir;
+};
+
+void tree_rec_alt(struct iso_dir *file, char *cur, struct treestruct tree,
+		  struct record *rec)
+{
+    if (tree.dir == '-')
+    {
+	rec->files += 1;
+	int len = 0;
+	for (; tree.name[len] != ';'; len++)
+	{
+	    continue;
+	}
+	tree.name_len = len;
+    }
+    else
+    {
+	rec->dir += 1;
+    }
+    if (tree.prof || !is_last_file(file, cur))
+	printf("|");
+    for (int i = 0; i < tree.prof; i++)
+    {
+	printf("   ");
+    }
+    if (is_last_file(file, cur))
+    {
+	for (int i = 0; i < tree.prof; i++)
+	{
+	    printf(" ");
+	}
+	printf("+-- %.*s/", tree.name_len, tree.name);
+    }
+    else
+    {
+	if (tree.prof && !is_last_file(file, cur))
+	{
+	    printf("|");
+	}
+	printf("-- %.*s", tree.name_len, tree.name);
+    }
+}
 
 void tree_rec(char *map, struct iso_dir *root, int prof, struct record *rec)
 {
     int count = 0;
     char *cur  = goto_file(map, root, 3);
+    struct treestruct treestruct;
+    treestruct.prof = prof;
     while (cur)
     {
         void *tmp = cur;
         struct iso_dir *file = tmp;
         if (!file->data_blk.le)
+	{
             return;
-        char *name = cur + sizeof(struct iso_dir);
-        char dir = file->type & 2 ? 'd' : '-';
-        int name_len = file->idf_len;
-        if (dir == '-')
-        {
-	    rec->files += 1;
-            int len = 0;
-            for (; name[len] != ';'; len++);
-            name_len = len;
         }
-	else
-	    rec->dir += 1;
-	if (prof || !is_last_file(file, cur))
-	    printf("|");
-	for (int i = 0; i < prof; i++)
-	{
-	    printf("   ");
-	}
-	if (is_last_file(file, cur))
-	{
-	    for (int i = 0; i < prof; i++)
-	    {
-		printf(" ");
-	    }
-	    printf("+-- %.*s/", name_len, name);
-	}
-	else
-	{
-	    if (prof && !is_last_file(file, cur))
-	    {
-		printf("|");
-	    }
-	    printf("-- %.*s", name_len, name);
-	}
+	treestruct.name = cur + sizeof(struct iso_dir);
+        treestruct.dir = file->type & 2 ? 'd' : '-';
+        treestruct.name_len = file->idf_len;
+	tree_rec_alt(file, cur, treestruct, rec);
 	printf("\n");
-	if (dir == 'd')
+	if (treestruct.dir == 'd')
+	{
 	    tree_rec(map, file, prof + 1, rec);
-        cur += file->dir_size;
+        }
+	cur += file->dir_size;
         count++;
     }
 }
@@ -64,7 +90,9 @@ void tree_func(char *map, struct iso_dir *root, struct input_params param)
     tmp_filename[len - 1] = '\0';
     char *cmd = strtok(tmp_filename, " ");
     if (!cmd)
+    {
 	printf(".\n");
+    }
     else
     {
 	printf("%s\n", cmd);
@@ -75,7 +103,10 @@ void tree_func(char *map, struct iso_dir *root, struct input_params param)
 	param.input = new;
 	root = cd_func(map, root, param, NULL);
     }
-    struct record rec = { 0, 0};
+    struct record rec =
+	{
+	    0, 0
+	};
     tree_rec(map, root, 0, &rec);
     printf("\n%d directories, %d files\n", rec.dir, rec.files);
 }
